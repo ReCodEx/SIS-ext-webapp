0b99e16752898c2393ef43985a66265ff1fc3197
Martin Kruliš <krulis@d3s.mff.cuni.cz>
2026-01-20 19:00:04 +0100

    Changing description fields to text areas in plant term groups form.

---
+++ src/components/forms/fields/TextAreaField.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form, FormControl, FormLabel } from 'react-bootstrap';
+import classnames from 'classnames';
+
+import * as styles from './commonStyles.less';
+
+const TextAreaField = ({
+  input,
+  meta: { active, dirty, error, warning },
+  type = 'text',
+  label = null,
+  children,
+  ignoreDirty = false,
+  className = '',
+  ...props
+}) => (
+  <>
+    {Boolean(label) && (
+      <FormLabel className={error ? 'text-danger' : warning ? 'text-warning' : undefined}>{label}</FormLabel>
+    )}
+    <FormControl
+      {...input}
+      {...props}
+      as="textarea"
+      rows={8}
+      isInvalid={Boolean(error)}
+      className={
+        classnames({
+          'form-control': true,
+          [styles.dirty]: dirty && !ignoreDirty && !error && !warning,
+          [styles.active]: active,
+          'border-danger': error,
+          'border-warning': !error && warning,
+        }) +
+        ' ' +
+        (className || '')
+      }
+    />
+    {error && <Form.Text className="text-danger"> {error} </Form.Text>}
+    {!error && warning && <Form.Text className="text-warning"> {warning} </Form.Text>}
+    {children}
+  </>
+);
+
+TextAreaField.propTypes = {
+  type: PropTypes.string,
+  input: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  children: PropTypes.any,
+  meta: PropTypes.shape({
+    active: PropTypes.bool,
+    dirty: PropTypes.bool,
+    error: PropTypes.any,
+    warning: PropTypes.any,
+  }).isRequired,
+  ignoreDirty: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+export default TextAreaField;
--- src/components/forms/fields/index.js
+++ src/components/forms/fields/index.js
@@ -4,3 +4,4 @@ export { default as NumericTextField } from './NumericTextField.js';
 export { default as SelectField } from './SelectField.js';
 export { default as StandaloneRadioField } from './StandaloneRadioField.js';
 export { default as TextField } from './TextField.js';
+export { default as TextAreaField } from './TextAreaField.js';
